b2768bb8fa009de86cdb3cdb8fc06e5706d7ac5b
Hara Chung <harachung90@gmail.com>
2022-12-07 19:37:10 +1100

    Seventh commit - enabled the badge text (sold out/online) on top of the images; learned to use conditional statement to display it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ReactDOM from "react-dom/client";
 import {Navbar} from "./Navbar";
 import {Hero} from "./Hero";
 import Card from "./Card";
@@ -9,48 +8,17 @@ import Data from "./Data";
 function App() {
     const cardElement = Data.map(card => {
         return <Card
-                id={card.id}
-                title={card.title}
-                description={card.description}
-                price={card.price}
-                coverImg={card.coverImg}
-                stats={card.stats}
-                location={card.location}
-                openSpots={card.openSpots}
-            />
+            key={card.id}
+            item={card}
+        />
     })
 
     return (
         <div className="App">
             <Navbar/>
             <Hero/>
-
             <div className="cards">
                 {cardElement}
-                {/*<Card
-                    img={katie}
-                    rating="5.0"
-                    raters="6"
-                    country="USA"
-                    title="Life lessons with Katie Zaferes"
-                    cost="136"
-                />
-                <Card
-                    img={wedding}
-                    rating="5.0"
-                    raters="30"
-                    country="USA"
-                    title="Learn wedding photography"
-                    cost="125"
-                />
-                <Card
-                    img={bike}
-                    rating="4.8"
-                    raters="2"
-                    country="USA"
-                    title="Group Mountain Biking"
-                    cost="50"
-                />*/}
             </div>
 
         </div>
--- src/Card.js
+++ src/Card.js
@@ -2,21 +2,28 @@ import React from 'react';
 import star from './star.png'
 
 export default function Card(props) {
+    let badgeText;
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <section className="container">
             <div className="card">
                 <div className="img_status">
-                    <p className="status">SOLD OUT</p>
-                    <img src={require(`./${props.coverImg}`)} alt={props.description}/>
+                    {badgeText && <p className="status">{badgeText}</p>}
+                    <img src={require(`./${props.item.coverImg}`)} alt={props.item.description}/>
                 </div>
                 <div className="card-info">
                     <img src={star} alt="star rating" className="star"/>
-                    <span>{props.stats.rating}</span>
-                    <span className="grey">({props.stats.reviewCount}) ﹒</span>
-                    <span className="grey">{props.location}</span>
+                    <span>{props.item.stats.rating}</span>
+                    <span className="grey">({props.item.stats.reviewCount}) ﹒</span>
+                    <span className="grey">{props.item.location}</span>
                 </div>
                 <p>{props.title}</p>
-                <p><span className="bold">From ${props.price} </span>/ person </p>
+                <p><span className="bold">From ${props.item.price} </span>/ person </p>
             </div>
         </section>
     )
